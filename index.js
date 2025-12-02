3ca0d8d758b05607dcb581c0900fd58b7e91343b
hk12214 <helenkokob317@gmail.com>
2025-12-02 16:10:09 +0300

    add search functionality by using  weather API

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+function refresh(response) {
+  let temperature = document.querySelector(".weather-app-temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector(".city");
+  cityElement.innerHTML = response.data.name;
+}
+function searchCity(city) {
+  let apikey = "a4d1ffe2f22e7da134fe0f4895a8bed3";
+  apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+  axios.get(apiurl).then(refresh);
+}
 function SearchSubmit(event) {
   event.preventDefault();
-
   let searchInput = document.querySelector(".search-form-input");
-  let cityElement = document.querySelector(".city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let formBtn = document.querySelector(".search-form");
