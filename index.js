bd4bffaa129dbe73c9b324982066745bb1f787e4
hk12214 <helenkokob317@gmail.com>
2025-12-03 11:15:36 +0300

    Enhance weather display with additional information
    Updated the refresh function to display weather, humidity, wind speed, and current date.

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 function refresh(response) {
   let temperature = document.querySelector(".weather-app-temperature");
-  temperature.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector(".city");
+  let weather = document.querySelector("#weather");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let currentDate = document.querySelector("#currentDate");
+  let date = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saterday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  temperature.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  weather.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = `${response.data.main.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  currentDate.innerHTML = `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apikey = "a4d1ffe2f22e7da134fe0f4895a8bed3";
